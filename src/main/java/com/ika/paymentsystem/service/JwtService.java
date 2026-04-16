9900dd097c7b5146e8917a0f2db75a44bedc499e
Irakli Evgenidze <iraklievgenidze17@gmail.com>
2026-04-16 23:18:37 +0400

    Add JWT service for token generation and parsing

---
+++ src/main/java/com/ika/paymentsystem/service/JwtService.java
@@ -0,0 +1,34 @@
+package com.ika.paymentsystem.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "very_secret_key_123";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
